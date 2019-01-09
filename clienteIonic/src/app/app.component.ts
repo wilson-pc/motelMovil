12c02311edfb1fe1448813f4476b71ce793643e8
wilson-pc <wilverhidalgo@outlook.com>
2019-01-09 14:35:47 -0400

    partes

--- clienteIonic/src/app/app.component.ts
+++ clienteIonic/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LocalWeatherPage } from '../pages/local-weather/local-weather';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      {title: 'Local Weather', component: LocalWeatherPage}
     ];
 
   }
@@ -40,5 +43,8 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+  logout() {
+    this.nav.setRoot(LoginPage);
   }
 }
--- clienteIonic/src/app/app.module.ts
+++ clienteIonic/src/app/app.module.ts
@@ -8,25 +8,69 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SettingsPage } from '../pages/settings/settings';
+import { CheckoutTripPage } from '../pages/checkout-trip/checkout-trip';
+import { LocalWeatherPage } from '../pages/local-weather/local-weather';
+import { NotificationsPage } from '../pages/notifications/notifications';
+import { LoginPage } from '../pages/login/login';
+import { TripDetailPage } from '../pages/trip-detail/trip-detail';
+import { RegisterPage } from '../pages/register/register';
+import { SearchLocationPage } from '../pages/search-location/search-location';
+import { TripsPage } from '../pages/trips/trips';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { ActivityService } from '../services/activity-service';
+import { TripService } from '../services/trip-service';
+import { WeatherProvider } from '../services/weather';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SettingsPage,
+    CheckoutTripPage,
+    LoginPage,
+    LocalWeatherPage,
+    NotificationsPage,
+    RegisterPage,
+    SearchLocationPage,
+    TripDetailPage,
+    TripsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    IonicModule.forRoot(MyApp,{
+      scrollPadding: false,
+      scrollAssist: true,
+      autoFocusAssist: false
+    }),
+    IonicStorageModule.forRoot({
+      name: '__ionic3_start_theme',
+        driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SettingsPage,
+    CheckoutTripPage,
+    LoginPage,
+    LocalWeatherPage,
+    NotificationsPage,
+    RegisterPage,
+    SearchLocationPage,
+    TripDetailPage,
+    TripsPage
   ],
   providers: [
     StatusBar,
+    ActivityService,
+    TripService,
+    WeatherProvider,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
