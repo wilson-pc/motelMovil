944d08f03379def22f128f5a1f28a56c258e7170
arodri <arodri.developer@gmail.com>
2019-01-18 16:33:37 -0400

    addig pages and structure

---
+++ Angular/src/app/pages/component/list-product/list-product.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-list-product',
+  templateUrl: './list-product.component.html',
+  styleUrls: ['./list-product.component.css']
+})
+export class ListProductComponent implements OnInit {
+  titulo: string;
+	public isCollapsed = true;
+	modal: NgbModalRef;
+	closeResult: string;
+
+	elements: any = [
+		{ apellidos: 'Castillo Rosas', nombres: 'Carla', ci: '96854885', genero: "otro", contacto: "45685222", email: "servicio@gmail.com", }
+		//user, pass
+	];
+	// Cabezeras de los elementos
+	headElements = ['Nro', 'Nombres', 'Apellidos', 'CI', 'Genero', 'Contacto', 'Email'];
+
+	constructor(private modalService: NgbModal) {
+		this.titulo = "Usuarios Administradores"
+	}
+
+	ngOnInit() { 
+		// funcion para cargar datos en la lista
+		this.getAdmin();
+	}
+
+	// ACCIONES DE LOS MODALS
+	openFromRegistry(content) {
+		this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })    
+    this.modal.result.then((e) => {
+    });  
+	}
+
+	openModalView(content) {
+		this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })    
+    this.modal.result.then((e) => {
+    });  
+	}
+	openModalUpdate(content) {
+		this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })    
+    this.modal.result.then((e) => {
+    });  
+	}
+
+	openModalDelete(content) {
+		this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })    
+    this.modal.result.then((e) => {
+    });  
+	}
+
+	cancelModal() {
+		this.modal.close();
+	}
+
+	// COSUMO DE SERVICIOS
+	add(){
+
+	}
+
+	update(){
+
+	}
+
+	delete(){
+
+	}
+	
+	getAdmin(){
+
+	}
+}
