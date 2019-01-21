b1c8dadfaeccddbd4ec6601a5f04e928b93fdd78
unknown <wilverhidalgo@outlook.com>
2019-01-21 10:07:18 -0400

    crud de usuarios creados en el back (aun sin probar con datos)

--- ServerNode/controllers/NegocioController.js
+++ ServerNode/controllers/NegocioController.js
@@ -1,42 +1,40 @@
 "use strict"
 var CryptoJS = require("crypto-js");
-var Producto = require("../schemas/producto");
-var clave=require("./../variables/claveCrypto");
-var Tipo=require("../schemas/tipo");
+var Negocio = require("../schemas/negocio");
+var TipoNegocio = require("../schemas/tipoNegocio");
+var clave=require("../variables/claveCrypto");
 module.exports = async function(io) {
 var clients = [];
   io.on('connection', async function (socket) {
-    console.log("tngourtoigyjhij");
    // var host=socket.handshake.headers.host;
     clients.push(socket.id);
     console.log("alguien se conecto");
 
-    socket.on('registrar-producto',async (data) => {
+    socket.on('registrar-negocio',async (data) => {
 
       try {
         const bytes = CryptoJS.AES.decrypt(data, clave.clave);
         if (bytes.toString()) {
           datos = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-          var producto = new Producto();
+          var negocio = new Producto();
         //  var tipo = new Tipo();
-          var params = datos.producto;
-          producto.nombre=params.nombre;
-          producto.negocio = params.negocio;
-          producto.precio=params.precio;
-          producto.disponibilidad=params.disponibilidad;
-          producto.cantidad=params.cantidad;
-          producto.tipo=await Tipo.findById(params.tito);
-          producto.foto=params.foto;
-          usuario.eliminado={estado:false,razon:""};
-          producto.descripcion=params.descripcion;
-          producto.creacion=params.creacion
-          producto.modificacion=params.modificacion;
-
-                  
-                      producto.save((error, nuevoProducto) => {
+          var params = datos.negocio;
+          negocio.nombre=params.nombre;
+          negocio.titular = params.titular;
+          negocio.foto=params.foto;
+          negocio.tipo=await TipoNegocio.findById(params.tiponegocio);
+          negocio.direccion=params.direccion;
+          negocio.telefono=params.telefono;
+          negocio.correo=params.correo;
+          negocio.nit=params.nit;
+          negocio.eliminado={estado:false,razon:""};
+          negocio.creacion=params.creacion
+          negocio.modificacion=params.modificacion;
+      
+                      negocio.save((error, nuevoProducto) => {
                           if (error) {
                   
-                              res.status(500).send({ mensaje: "error al guradar" })
+                          //    res.status(500).send({ mensaje: "error al guradar" })
                           } else {
                             io.emit('respuesta',nuevoProducto);  
                           }
@@ -60,7 +58,7 @@ var clients = [];
         io.emit('respuesta', {user: socket.nickname, event: 'left'});   
       });
 
-      socket.on('registrar-producto', async (data) => {
+      socket.on('registrar-negocio', async (data) => {
      
         io.emit('respuesta', {user: socket.nickname, event: 'left'});   
       });
