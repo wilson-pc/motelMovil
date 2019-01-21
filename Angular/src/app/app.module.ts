6c8cc960b5d1129746c34273ec39206adc21e825
arodri <arodri.developer@gmail.com>
2019-01-21 13:46:45 -0400

    Refactor WEB CONTENT

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import * as $ from 'jquery';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Socket, SocketIoModule } from 'ngx-socket-io';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
