b926c01644ce044baf9fa4c6823ff7a6e4ce7f9f
unknown <wilverhidalgo@outlook.com>
2019-01-21 10:52:36 -0400

    crud negocios en le back (sin probar)

--- ServerNode/controllers/NegocioController.js
+++ ServerNode/controllers/NegocioController.js
@@ -87,11 +87,110 @@ var clients = [];
       }
       });
 
-      socket.on('registrar-negocio', async (data) => {
      
-        io.emit('respuesta', {user: socket.nickname, event: 'left'});   
+      //
+      //
+      //
+      //
+      socket.on('eliminar-negocio', async (data) => {
+     
+        try {
+            const bytes = CryptoJS.AES.decrypt(data, clave.clave);
+            if (bytes.toString()) {
+             var datos = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+              var negocio = new Negocio();
+              var params = datos.negocio;
+              negocio._id=params._id;
+              negocio.eliminado={estado:true,razon:params.razon},
+                          //guarda al nuevo usuario en la bd
+                      
+                          Negocio.findByIdAndUpdate(params.id,negocio,{new: true},(error, actualizado) => {
+                              if (error) {
+                      
+                                 // res.status(500).send({ mensaje: "error al guradar" })
+                              } else {
+                                io.emit('respuesta',actualizado);  
+                              }
+                          })
+                    
+            }
+           else{
+
+           }
+          } catch (e) {
+            console.log(e);
+          }
+        
+      });
+
+      socket.on('listar-negocio', async (data) => {
+      Negocio.find({"eliminado.estado":false}, function (error, lista){
+        if (error) {
+           // res.status(500).send({ mensaje: "Error al listar" })
+        } else {
+            if (!lista) {
+             //   res.status(404).send({ mensaje: "Error al listar" })
+            } else {
+                io.emit('respuesta',lista);  
+            }
+        }
+      });
       });
      
+      
+      socket.on('sacar-negocio', async (data) => {
+        try {
+            const bytes = CryptoJS.AES.decrypt(data, clave.clave);
+            if (bytes.toString()) {
+             var datos = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+               
+                
+        Negocio.findOne({_id:datos.id,"eliminado.estado":false}, function (error, dato){
+            if (error) {
+               // res.status(500).send({ mensaje: "Error al listar" })
+            } else {
+                if (!lista) {
+                 //   res.status(404).send({ mensaje: "Error al listar" })
+                } else {
+                    io.emit('respuesta',dato);  
+
+                }
+            }
+          });
+            
+            }
+            return data;
+          } catch (e) {
+            console.log(e);
+          }   
+
+        });
+        socket.on('buscar-usuario', async (data) => {
+            try {
+                const bytes = CryptoJS.AES.decrypt(data, clave.clave);
+                if (bytes.toString()) {
+                 var datos = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+                   
+                    
+            Negocio.find({"eliminado.estado":false,nombre:datos.termino,apellido:datos.nit}, function (error, lista){
+                if (error) {
+                   // res.status(500).send({ mensaje: "Error al listar" })
+                } else {
+                    if (!lista) {
+                     //   res.status(404).send({ mensaje: "Error al listar" })
+                    } else {
+                        io.emit('respuesta',lista);  
+                    }
+                }
+              });
+                
+                }
+                return data;
+              } catch (e) {
+                console.log(e);
+              }   
+    
+            });
 })
 
 
