40fe763e03e4cfa062a1c50e48bdeaec66b95425
arodri <arodri.developer@gmail.com>
2019-01-23 12:00:08 -0400

    adding services to commerce

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
 import { SpinnerComponent } from './shared/spinner.component';
 import { WebComponent } from './web/web.component';
 import { LoginComponent } from './login/login.component';
-import { SocketConfigService, SocketConfigService2 } from './socket-config.service';
+import { SocketConfigService, SocketConfigService2, SocketConfigService3 } from './socket-config.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +29,7 @@ import { SocketConfigService, SocketConfigService2 } from './socket-config.servi
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [SocketConfigService,SocketConfigService2,
+  providers: [SocketConfigService,SocketConfigService2,SocketConfigService3,
     {
       provide: LocationStrategy,
       useClass: PathLocationStrategy
