7f2d23eae5d516519b50f202785a5ab085895baa
arodri <arodri.developer@gmail.com>
2019-01-24 09:39:20 -0400

    Add function Complaint

--- Angular/src/app/pages/complaint/complaint.component.ts
+++ Angular/src/app/pages/complaint/complaint.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-complaint',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complaint.component.css']
 })
 export class ComplaintComponent implements OnInit {
+  titulo: string;
+	public isCollapsed = true;
+	modal: NgbModalRef;
+	closeResult: string;
 
-  constructor() { }
-
-  ngOnInit() {
+	elements: any = [
+		{  producto: 'Rom Abuelo', nombreNegocio: 'LicoreriaCarla', negocio: 'Licoreria', admin: 'Carla', cantidadDenuncias: '65'}
+		//user, pass
+	];
+	// Cabezeras de los elementos
+	headElements = ['Nro', 'Producto', 'Negocio', 'Tipo Negocio', 'Administrador', 'Denuncias'];
+  constructor(private modalService: NgbModal) { 
+    this.titulo = "ADMINISTRACION DE DENUNCIAS"
   }
 
+	ngOnInit() { 
+		// funcion para cargar datos en la lista
+		this.getAdmin();
+	}
+
+	// ACCIONES DE LOS MODALS
+
+	openModalView(content) {
+		this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })    
+    this.modal.result.then((e) => {
+    });  
+	}
+
+	openModalDelete(content) {
+		this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })    
+    this.modal.result.then((e) => {
+    });  
+	}
+
+	cancelModal() {
+		this.modal.close();
+	}
+
+	// COSUMO DE SERVICIOS
+
+	delete(){
+
+	}
+	
+	getAdmin(){
+
+	}
 }
