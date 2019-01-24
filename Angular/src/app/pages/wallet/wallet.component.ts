7ace8ae8e2bdc60b25ad109ccf49a971ee919d81
arodri <arodri.developer@gmail.com>
2019-01-24 12:13:06 -0400

    Addign module design Wallet

--- Angular/src/app/pages/wallet/wallet.component.ts
+++ Angular/src/app/pages/wallet/wallet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { UsuarioService } from '../../services/usuario.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-wallet',
@@ -7,9 +10,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+  public isCollapsed = true;
+  modal: NgbModalRef;
+  closeResult: string;
+  isError: boolean = false;
+  isExito: boolean = false;
+  isRequired: boolean = false;
+
+  saldo = '150';
+  today = new Date();
+  hour = '';
+
+  // Cabeceras de la Tabla
+  usuarios: any = [
+    {
+      nombre: 'Carla',
+      apellidos: 'Castillo Flores',
+      tipo: 'admin',
+      saldo: '1500'
+    },
+    {
+      nombre: 'Rosio',
+      apellidos: 'Castillo Flores',
+      tipo: 'user',
+      saldo: '150'
+    },
+  ];
+  headElements = ['Nro', 'Nombres', 'Apellidos', 'Tipo Usuario', 'Saldo'];
+
+  items: any = []
+  term: string;
+  validUser: string;
+
+  constructor(private modalService: NgbModal, private usuarioServ: UsuarioService) {
+    this.titulo = "ADMINISTRACION DE USUARIOS";
+    // Hora actual
+    this.hour = formatDate(this.today, 'dd-MM-yyyy hh:mm a', 'en-US');
+    this.validUser = '';
+  }
 
   ngOnInit() {
   }
 
+  // ACCIONES DE LOS MODALS
+  openModalView(content) {
+    this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })
+    this.modal.result.then((e) => {
+    });
+  }
+
+  openModalAddMoney(content) {
+    this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })
+    this.modal.result.then((e) => {
+    });
+  }
+
+  openModalLeaveMoney(content) {
+    this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })
+    this.modal.result.then((e) => {
+    });
+  }
+
+  openModalFormMoney(content, user: any) {
+    this.modal = this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' })
+    this.modal.result.then((e) => {
+    });
+    this.validUser = user.tipo;
+  }
+
+  cancelModal() {
+    this.modal.close();
+  }
+
+  // COSUMO DE SERVICIOS
+  delete() {
+
+  }
+
+  suspend() {
+
+  }
 }
