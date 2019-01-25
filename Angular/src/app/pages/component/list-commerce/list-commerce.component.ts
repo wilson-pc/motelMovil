ad6080f4324291d08525439d058d4db461e32d8e
arodri <arodri.developer@gmail.com>
2019-01-25 12:41:30 -0400

    edit listcommerce page

--- Angular/src/app/pages/component/list-commerce/list-commerce.component.ts
+++ Angular/src/app/pages/component/list-commerce/list-commerce.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { StarterComponent } from '../../starter/starter.component';
+import { Negocio } from '../../../models/Negocio';
+import { SocketConfigService3 } from '../../../socket-config.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -10,14 +13,23 @@ import { StarterComponent } from '../../starter/starter.component';
 })
 export class ListCommerceComponent implements OnInit {
   private tipoNegocio: string = '';
+  listCommerce: Negocio[];
+  closeResult: string;
+  isError: boolean = false;
+  isExito: boolean = false;
+  isRequired: boolean = false;
+  negocios: Negocio;
+
+
   elements: any = [
-    { nombre: 'Licoreria Castillo', 
-      cantidadProductos: 'Carla', 
+    {
+      nombre: 'Licoreria Castillo',
+      cantidadProductos: 'Carla',
       valoracion: '5',
-      cantidadReportes: '10', 
+      cantidadReportes: '10',
       cantidadVisitas: "1200",
-      montoTotal: "3600", 
-      montoMesPasado: "3000", 
+      montoTotal: "3600",
+      montoMesPasado: "3000",
       montoActual: "600",
     }
     //user, pass
@@ -25,30 +37,51 @@ export class ListCommerceComponent implements OnInit {
   // Cabezeras de los elementos
   headElements = ['Nro', 'Nombre', 'Cantidad Productos', 'Valoracion', 'Reportes', 'Visitas', 'Monto Total', 'Monto Mes Pasado', 'Monto Actual'];
 
-  constructor(private rout: Router, private route:ActivatedRoute) {
+  constructor(private rout: Router, private route: ActivatedRoute, private socketNegocio: SocketConfigService3) {
     this.tipoNegocio = this.route.snapshot.paramMap.get('tipo');
+    this.listCommerce = [];
+    this.getCommerces();
   }
 
   ngOnInit() {
-    // funcion para cargar datos en la lista
-    this.getAdmin();
+    console.log("Array");
+    console.log(this.listCommerce);
   }
 
 
   // COSUMO DE SERVICIOS
-  getAdmin() {
-
+  getCommerces() {
+    this.socketNegocio.emit("listar-negocio", { data: "nada" });
   }
 
-  openListProducts(commerce: any){
+  conn() {
+    this.listCommerce = [];
+    this.respuestaListarNegocio().subscribe((data: any[]) => {
+      console.log("carga al array de negocios");
+      this.listCommerce = data;
+      console.log(this.negocios)
+    });
+  }
 
-    if(this.tipoNegocio === 'moteles'){
+  respuestaListarNegocio() {
+    let observable = new Observable(observer => {
+      this.socketNegocio.on('respuesta-listar-negocio', (data) => {
+        observer.next(data);
+      });
+    })
+    return observable;
+  }
+
+  // REDIRECCION A RUTAS SEGUN EVENTO
+  openListProducts(commerce: any) {
+
+    if (this.tipoNegocio === 'moteles') {
       this.rout.navigate(['/administracion/moteles/' + commerce.nombre]);
     }
-    if(this.tipoNegocio === 'licorerias'){
+    if (this.tipoNegocio === 'licorerias') {
       this.rout.navigate(['/administracion/licorerias/' + commerce.nombre]);
     }
-    if(this.tipoNegocio === 'sexshops'){
+    if (this.tipoNegocio === 'sexshops') {
       this.rout.navigate(['/administracion/sexshops/' + commerce.nombre]);
     }
   }
