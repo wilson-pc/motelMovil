1a8af983784550e33082f0d8a77c7c7d2b46a8b6
arodri <arodri.developer@gmail.com>
2019-01-28 12:50:00 -0400

    delete line path route

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { WebComponent } from './web/web.component';
 import { LoginComponent } from './login/login.component';
-import { Error404Component } from './pages/error404/error404.component';
 
 const routes: Routes = [
     {
@@ -24,8 +23,7 @@ const routes: Routes = [
         path: '',
         loadChildren: './pages/pages.module#PagesModule'
     },
-    { path: 'login', component: LoginComponent },
-    { path: '/*path', component: Error404Component}
+    { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
