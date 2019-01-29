d1c5135a238e62da3797258867d485441ee9701f
unknown <wilverhidalgo@outlook.com>
2019-01-29 14:20:12 -0400

    socket istalado y configurado en ionic

---
+++ ClienteAdminIonic/src/providers/socket-config/socket-config.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Socket } from 'ng-socket-io';
+import { RutaServer } from '../../app/RutaApi';
+//const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
+
+
+@Injectable()
+export class SocketConfigService extends Socket {
+  constructor() {
+
+    super({ url: RutaServer.socket + "productos", options: {} });
+  }
+
+}
+
+export class SocketConfigService2 extends Socket {
+  constructor() {
+    super({ url: RutaServer.socket + "usuarios", options: {} });
+  }
+}
+
+export class SocketConfigHomeService extends Socket {
+  constructor() {
+    super({ url: RutaServer.socket + "home", options: {} });
+  }
+}
+export class SocketConfigService3 extends Socket {
+  constructor() {
+    super({
+      url: RutaServer.socket + "negocios", options: {}
+    })
+  }
+}
