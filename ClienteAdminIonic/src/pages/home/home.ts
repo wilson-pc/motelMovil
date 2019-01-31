01840aff1faad623309e03932b1bb1eb23bf5d45
arodri <arodri.developer@gmail.com>
2019-01-31 09:51:33 -0400

    adding fucntion HomePage

--- ClienteAdminIonic/src/pages/home/home.ts
+++ ClienteAdminIonic/src/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
 import { Negocio } from "../../models/Negocio";
-import { Usuarios } from "../../models/Usuarios";
 import { UserOnlyProvider } from "../../providers/user-only/user-only";
+import { SocketServiceCommerce } from "../../providers/socket-config/socket-config";
+import { Observable } from "rxjs";
+import * as CryptoJS from 'crypto-js';
+import { clave } from "../../app/cryptoclave";
 
 
 @Component({
@@ -13,29 +16,53 @@ import { UserOnlyProvider } from "../../providers/user-only/user-only";
 export class HomePage {
 
   listCommerce: Negocio[] = [];
-  userOnly: Usuarios;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public userOnlyProvider: UserOnlyProvider) {
-
-    this.getUserOnly()
+    public userOnlyProvider: UserOnlyProvider,
+    public productService: SocketServiceCommerce) {
+      //Inicializacion
+    this.connectionBackendSocket();
+    this.getAllCommerce();
   }
 
   ionViewWillEnter() {
 
   }
 
-  getUserOnly() {
-    console.log("usuario ingreso ++");
-    console.log(this.userOnlyProvider.userSesion);
+  doRefresh(event) {
+    console.log('Comenzar la operación asíncrona');
+    //this.getAllCommerce();
+    setTimeout(() => {
+      console.log('La operación asíncrona ha finalizado');
+      this.getAllCommerce();
+      event.complete();
+    }, 2000);
   }
 
   //Consumos de Servicios
   getAllCommerce() {
-
+    // Consulta cantidad de negocios
+    let data = { id: this.userOnlyProvider.userSesion.datos._id, tipo: "negocios" }
+		var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), clave.clave);
+		this.productService.emit("listar-negocios-de-usuario", ciphertext.toString());
   }
-}
 
-//
+  // Conexion con el Backend
+  connectionBackendSocket() {
+    this.respuestaVerificarNegocio().subscribe((data: any) => {
+      this.listCommerce = data;
+			console.log("Negocios: ", this.listCommerce);
+		});
+  }
+
+  respuestaVerificarNegocio() {
+		let observable = new Observable(observer => {
+			this.productService.on('respuesta-listar-negocio-de-usuario', (data) => {
+				observer.next(data);
+			});
+		})
+		return observable;
+	}
+}
