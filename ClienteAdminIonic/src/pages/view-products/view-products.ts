d66e2bab28efc5ba3f2e78b45403d0d82ff3745e
arodri <arodri.developer@gmail.com>
2019-02-08 10:39:03 -0400

    Design and development of the product information page

---
+++ ClienteAdminIonic/src/pages/view-products/view-products.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { Negocio } from '../../models/Negocio';
+import { Productos } from '../../models/Productos';
+
+@Component({
+  selector: 'page-view-products',
+  templateUrl: 'view-products.html',
+})
+export class ViewProductsPage {
+
+  commerceOnly: Negocio;
+  productOnly: Productos;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController, ) {
+    //Inicializacion
+    this.getProduct();
+  }
+
+  ionViewDidLoad() {
+  }
+
+  getProduct() {
+    this.productOnly = this.navParams.get('product');
+    this.commerceOnly = this.navParams.get('commerce');
+  }
+
+}
