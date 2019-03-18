008e96467a046e657a402f4e0dbea6d9e427aa31
arodri <arodri.developer@gmail.com>
2019-03-18 14:18:08 -0400

    Cambiar ruta a heroku

--- ClienteAdminIonic/src/app/RutaApi.ts
+++ ClienteAdminIonic/src/app/RutaApi.ts
@@ -1,4 +1,5 @@
 export var RutaServer = {
-    socket: "http://192.168.1.5:3000/",
+    socket: "https://triservicedemo.herokuapp.com/",
+    //socket: "http://192.168.1.5:3000/",
     api:"http://localhost:3000/api/"
 }
