1ceb5ad8d029fa074f52c0e63dd8bdb5f58983e8
arodri <arodri.developer@gmail.com>
2019-03-20 10:27:21 -0400

    redireccion de ip servidor

--- ClienteAdminIonic/src/app/RutaApi.ts
+++ ClienteAdminIonic/src/app/RutaApi.ts
@@ -3,5 +3,5 @@ export var RutaServer = {
     //socket: "http://192.168.1.5:3000/",
     api:"http://localhost:3000/api/",
     
-    socket: "http://192.168.1.15:3000/"
+    socket: "http://192.168.1.14:3000/"
 }
