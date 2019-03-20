f6185faf0190c191d31ca90655ac6915e2a75f52
Anghelo Rodriguez Apaza <acherfun99@gmail.com>
2019-03-20 11:07:44 -0400

    borrando api key

--- clienteIonic/src/services/RutaApi.ts
+++ clienteIonic/src/services/RutaApi.ts
@@ -1,7 +1,7 @@
 export var RutaServer = {
     //socket: "http://localhost:3000/",
     api:"http://localhost:3000/api/",
-    //api google:AIzaSyBE3HlYio93MSzINEaZdQvz9PunGDvBOf0
+    
     
     socket: "http://192.168.1.14:3000/"
 }
