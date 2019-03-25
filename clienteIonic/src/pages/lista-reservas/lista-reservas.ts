294ad269b70cf1bc96920c32a3d77f1c9861e6a4
arodri <arodri.developer@gmail.com>
2019-03-25 16:44:16 -0400

    Diseño E Interfaz de la pagina reservas

--- clienteIonic/src/pages/lista-reservas/lista-reservas.ts
+++ clienteIonic/src/pages/lista-reservas/lista-reservas.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ListaReservasLicoresPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ListaReservasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tipo = "Reserva";
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams) {
+    // Inicializacion
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ListaReservasLicoresPage');
+    console.log('');
   }
 
 }
